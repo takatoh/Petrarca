74a18d1983d0527e6f38711e1dbeb18bbae7dcf8
takatoh <takatoh.m@gmail.com>
2020-08-20 14:50:17 +0900

    lib/isnb_utils/isbn10.rb(calc_check_digit): New.

--- lib/isbn_utils/isbn10.rb
+++ lib/isbn_utils/isbn10.rb
@@ -7,5 +7,20 @@ module ISBNUtils
       isbn = isbn.delete("-")
       !!(/\A\d{9}[0-9X]\z/ =~ isbn)
     end
+
+    def calc_check_digit(isbn)
+      nums = isbn.delete("-").split("")[0..8].map{|x| x.to_i }
+      sum = nums.zip((2..10).to_a.reverse).map{|x, y| x * y }.inject(:+)
+      check_digit = 11 - (sum % 11)
+      case check_digit
+      when 10
+        "X"
+      when 11
+        "0"
+      else
+        check_digit.to_s
+      end
+    end
+
   end
 end
