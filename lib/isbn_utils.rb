d308c4de133cc9feb1b4d18168befbf295d6bb14
takatoh <takatoh.m@gmail.com>
2020-08-20 14:18:58 +0900

    lib/isbn_utils.rb(ISBNUtils.correct_format?): New.

--- lib/isbn_utils.rb
+++ lib/isbn_utils.rb
@@ -2,5 +2,18 @@ require "isbn_utils/version"
 
 module ISBNUtils
   class Error < StandardError; end
-  # Your code goes here...
+
+  extend self
+
+  def correct_format?(isbn)
+    isbn = isbn.delete("-")
+    case isbn.size
+    when 13
+      !!(/\A97[89]\d{9}\d\z/ =~ isbn)
+    when 10
+      !!(/\A\d{9}[0-9X]\z/ =~ isbn)
+    else
+      false
+    end
+  end
 end
