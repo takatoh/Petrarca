33e19249fab2186fae0c3a0a9485bef3dbcf3e56
takatoh <takatoh.m@gmail.com>
2020-08-29 20:47:33 +0900

    spec/helpers_spec.rb: New. Tests for ISBNUtils::Helpers module.

---
+++ spec/helpers_spec.rb
@@ -0,0 +1,23 @@
+require_relative "../lib/isbn_utils"
+
+
+include ISBNUtils::Helpers
+
+RSpec.describe ISBNUtils::Helpers do
+
+  let(:isbn13) { "9784815606442" }
+  let(:isbn13_hyphenated) { "978-4-8156-0644-2" }
+
+  describe "split_to_parts" do
+    it "returns a Array include prefix and body" do
+      expect(split_to_parts(isbn13.sub(/^978/, ""), ISBNUtils::REGISTRATION_GROUP_RANGES["978"])).to eq ["4", "815606442"]
+    end
+  end
+
+  describe "hyphenate13" do
+    it "returns hyphenated isbn13" do
+      expect(hyphenate13(isbn13)).to eq isbn13_hyphenated
+    end
+  end
+
+end
