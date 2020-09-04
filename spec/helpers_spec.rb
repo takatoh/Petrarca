9174bae8e954199aeab27733cac29ed67bd4ed8c
takatoh <takatoh.m@gmail.com>
2020-09-05 06:23:48 +0900

    spec/helpers_spec.rb: Tweak.

--- spec/helpers_spec.rb
+++ spec/helpers_spec.rb
@@ -5,13 +5,9 @@ include Petrarca::Helpers
 
 RSpec.describe Petrarca::Helpers do
 
-  let(:isbn13) { "9784815606442" }
-  let(:isbn13_hyphenated) { "978-4-8156-0644-2" }
-
   describe "split_to_parts" do
     it "returns a Array include prefix and body" do
-      expect(split_to_parts(isbn13.sub(/^978/, ""), Petrarca::REGISTRATION_GROUP_RANGES["978"])).to eq ["4", "815606442"]
-    end
+      expect(split_to_parts("4815606442", Petrarca::REGISTRATION_GROUP_RANGES["978"])).to eq ["4", "815606442"]    end
   end
 
 end
