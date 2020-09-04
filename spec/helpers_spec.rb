96db427b0733fee50d237a6faf97046e119a40b9
takatoh <takatoh.m@gmail.com>
2020-09-05 06:28:46 +0900

    spec/helpers_spec.rb: Prepare to change Petrarca::Helpers module.

--- spec/helpers_spec.rb
+++ spec/helpers_spec.rb
@@ -1,13 +1,11 @@
 require_relative "../lib/petrarca"
 
 
-include Petrarca::Helpers
-
 RSpec.describe Petrarca::Helpers do
 
   describe "split_to_parts" do
     it "returns a Array include prefix and body" do
-      expect(split_to_parts("4815606442", Petrarca::REGISTRATION_GROUP_RANGES["978"])).to eq ["4", "815606442"]    end
+      expect(Petrarca::Helpers.split_to_parts("4815606442", Petrarca::REGISTRATION_GROUP_RANGES["978"])).to eq ["4", "815606442"]    end
   end
 
 end
