8eafc5ce69217ea93feb0644cb431dc51d4e8a2a
takatoh <takatoh.m@gmail.com>
2022-05-03 06:03:03 +0900

    lib/petrarca/helpers.rb(split): New. Moved from lib/petrarca/isbn13.rb.

--- lib/petrarca/helpers.rb
+++ lib/petrarca/helpers.rb
@@ -3,6 +3,18 @@ module Petrarca
 
     extend self
 
+    def split(isbn)
+      isbn = isbn.to_s.delete("-")
+      ean_prefix = isbn[0, 3]
+      body = isbn[3, 9]
+      check_digit = isbn[12, 1]
+      registration_group, body = Helpers.split_to_parts(body, REGISTRATION_GROUP_RANGES[ean_prefix])
+      prefix = "#{ean_prefix}-#{registration_group}"
+      registrant, publication = Helpers.split_to_parts(body, REGISTRANT_RANGES[prefix])
+      [ean_prefix, registration_group, registrant, publication, check_digit]
+    end
+
+
     def split_to_parts(body, ranges)
       ranges.map do |range_str|
         s, e = range_str.split("-")
