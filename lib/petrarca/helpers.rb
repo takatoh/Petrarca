27bbc8798df899637796f107ca7f257c964f3a8e
takatoh <takatoh.m@gmail.com>
2024-08-14 18:01:14 +0900

    lib/petrarca/helpers.rb(load_ranges): Removed.

--- lib/petrarca/helpers.rb
+++ lib/petrarca/helpers.rb
@@ -47,18 +47,5 @@ module Petrarca
       end
     end
 
-
-    def load_ranges(range_file)
-      ranges = {}
-      File.open(range_file, "r") do |f|
-        f.each_line do |line|
-          next if line.start_with?("#")
-          g, r = line.chomp.split(":")
-          ranges[g] = r.split(",") unless r.nil?
-        end
-      end
-      ranges
-    end
-
   end
 end
